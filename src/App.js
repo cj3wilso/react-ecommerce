3df4ba623b10dea39279886f45f38510ffbd7e3a
cj4wilso <cj3wilso@gmail.com>
2023-01-11 20:12:30 +0000

    mobile friendly css and global styles

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ import { Routes, Route } from "react-router-dom";
 import Spinner from "./components/spinner/spinner.component";
 import { checkUserSession } from "./store/user/user.action";
 
-const Navigation = lazy(()=>import("./routes/navigation/navigation.component"));
-const Home = lazy(()=>import("./routes/home/home.component"));
-const Authentication = lazy(()=>import("./routes/authentication/authentication.component"));
-const Shop = lazy(()=>import("./routes/shop/shop.component"));
-const Checkout = lazy(()=>import("./routes/checkout/checkout.component"));
+import { GlobalStyle } from "./global.styles";
+
+const Navigation = lazy(() =>
+  import("./routes/navigation/navigation.component")
+);
+const Home = lazy(() => import("./routes/home/home.component"));
+const Authentication = lazy(() =>
+  import("./routes/authentication/authentication.component")
+);
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -20,16 +26,17 @@ const App = () => {
   }, []);
 
   return (
-    <Suspense fallback={<Spinner/>}>
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="shop" element={<Shop />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
     </Suspense>
   );
 };
